7aefa41ff7a13a691f4e2b9c443b262722d1e838
Sam Skinner <sam.sjskinner@gmail.com>
2021-01-12 17:24:51 +1100

    updated user_controller to accept strong params

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-
+  #
   get 'users/new'
   get 'users/create'
   post 'user_token' => 'user_token#create'
   resources :restaurants
+  resources :users
   get '/search' => 'restaurants#search'
 
   resources :menus
