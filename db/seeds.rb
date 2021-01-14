912e42795beeaf148d8fcf6f5a2fdd413beaefba
Sam Skinner <sam.sjskinner@gmail.com>
2021-01-14 12:18:33 +1100

    users controller now nests restaurants and menus data in json response to control panel

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,10 +10,14 @@ puts "created #{User.count} users"
 puts 'Creating new restaurants'
 Restaurant.destroy_all
 
-r1 = Restaurant.create! name: 'Tiamo', address: '1 lygon st', cuisine: 'italian', user_id: u1.id
-r2 = Restaurant.create! name: 'Gigis', address: '50 king st', cuisine: 'italian', user_id: u2.id
-r3 = Restaurant.create! name: 'Pho Phd', address: '1 merickville st', cuisine: 'vietnamese', user_id: u3.id
-r4 = Restaurant.create! name: 'Soul Burger', address: '229A King St, Newtown, NSW, 2042', cuisine: 'American', user_id: u2.id
+r1 = Restaurant.create! name: 'Tiamo', address: '1 lygon st', cuisine: 'italian'
+r2 = Restaurant.create! name: 'Gigis', address: '50 king st', cuisine: 'italian'
+r3 = Restaurant.create! name: 'Pho Phd', address: '1 merickville st', cuisine: 'vietnamese'
+r4 = Restaurant.create! name: 'Soul Burger', address: '229A King St, Newtown, NSW, 2042', cuisine: 'American'
+
+u1.restaurants << r1
+u2.restaurants << r2 << r4
+u3.restaurants << r3
 
 puts "created #{Restaurant.count} restaurants"
 
